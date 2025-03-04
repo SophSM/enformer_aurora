40563b5ebec644764092c702aadc31960b6f4d65
rbonazzola <rodbonazzola@gmail.com>
2025-03-04 18:29:52 +0000

    Added setup.py to make this into a package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="enformer-polaris",
+  version="0.0.1",
+  author="Rodrigo Bonazzola (rbonazzola)",
+  author_email="rodbonazzola@gmail.com",
+  description="Python package for running Enformer on ALCF's Polaris.",
+  long_description=open("README.md", encoding="utf-8").read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/rbonazzola/enformer",
+  packages=find_packages(),
+  install_requires=[
+    "mlflow"      
+  ],
+  python_requires=">=3.8,<3.11"
+)
+

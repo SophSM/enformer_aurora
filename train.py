2a4b8e11afc004ff32ce7343199f5ae7e4fcf6bd
SophSM <sogusama02@gmail.com>
2025-05-20 12:30:01 -0500

    pattern state dict

--- train.py
+++ train.py
@@ -69,8 +69,8 @@ def build_model_and_optimizer(enformer_params, from_checkpoint, ckpt_dir, _devic
 
 
         state_dict = ckpt['model_state_dict']
-        unwanted_prefix = '_orig_mod.module.'
-        for k, _ in list(state_dict.items()):
+        unwanted_prefix = 'module.'
+        for k in list(state_dict.keys()):
             if k.startswith(unwanted_prefix):
                 state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
         model.load_state_dict(state_dict)
